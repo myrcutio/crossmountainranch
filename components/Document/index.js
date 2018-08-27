25b3ba110cf0cfa35d8675efe50c488f1dcbc43b
Stephanie Yang <steppy452@gmail.com>
2018-08-27 13:17:49 -0400

    Fix board and document list styling

--- components/Document/index.js
+++ components/Document/index.js
@@ -1,10 +1,9 @@
 export default ({data}) => (
-  <div>
-    { data.docTitle ? <h2>{data.docTitle}</h2> : null }
-    { data.docUrl ? <ul>
-      <li>
+  <div className={ data.docTitle ? 'document-section' : null }>
+    { data.docTitle ? <h2 className="document-section-label">{data.docTitle}</h2> : null }
+    { data.docUrl ?
+      <div className="document-item">
         <a href={data.docUrl}>{data.docLabel}</a>
-      </li>
-    </ul> : null }
+      </div> : null }
   </div>
 )
